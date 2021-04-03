8bbe21737ab40417711cdc16e932c5268fc10db2
Arya Anand Sagar <39986104+rapunzel99@users.noreply.github.com>
2021-04-03 23:53:00 +0530

    third commit

--- login.js
+++ login.js
@@ -1,8 +1,40 @@
 let inputEmail = document.getElementById("inputEmail");
-// let inputPassword = document.getElementById("inputPassword");
+let inputPassword = document.getElementById("inputPassword");
 let error = document.getElementById("error");
 
+
+function emailValidator(email) {
+    var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email.match(emailRegex)) {
+        document.getElementById("errorTextEmail").innerHTML = "Valid";
+        document.getElementById("errorTextEmail").style.visibility = "visible";
+        document.getElementById("errorTextEmail").style.color = "green";
+        return true;
+    } else {
+        document.getElementById("errorTextEmail").innerHTML = "Invalid";
+        document.getElementById("errorTextEmail").style.visibility = "visible";
+        document.getElementById("errorTextEmail").style.color = "red";
+        return false;
+    }
+}
+
+email.onkeyup = function () {
+    isEmailValid = emailValidator(email.value);
+}
 function validate(){
+    return  isEmailValid && isPasswordValid;
+}
+
+
+
+
+
+
+
+
+
+
+// function validate(){
     //    if(inputEmail.value.trim() ==""||inputPassword.value.trim() ==""){
     //        alert("Fields cannot be empty");
     //        return false;
@@ -13,17 +45,17 @@ function validate(){
     //     }
 
 
-    let regexp = /^([A-Za-z0-9\.-])@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-    if(regexp.test(inputEmail.value)){
-        error.innerHTML = "valid";
-        error.style.color ="green";
-        return true;
-    }
-    else{
-        error.innerHTML=" email invalid";
-        error.style.color="red";
-        return false;
-    }
+    // let regexp = /^([A-Za-z0-9\.-])@([A-za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    // if(regexp.test(inputEmail.value)){
+    //     error.innerHTML = "valid";
+    //     error.style.color ="green";
+    //     return true;
+    // }
+    // else{
+    //     error.innerHTML=" email invalid";
+    //     error.style.color="red";
+    //     return false;
+    //}
     // else if(inputPassword.value.length<=5){
     //     alert("Password is too short");
     //     inputPassword.style.border="2px soild red";
@@ -34,4 +66,4 @@ function validate(){
     // }
 
 
-    }
+    //}
